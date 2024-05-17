70c0e26accf51bba6ec74e4bed90c249a41c4a7a
MohannadMalayo <realnoody@outlook.com>
2024-05-18 00:37:38 +0300

    Hello world

--- GraphQL/server.js
+++ GraphQL/server.js
@@ -2,7 +2,61 @@ const express = require('express');
 const app = express();
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
+const fetch = require('node-fetch');
 
+const schema = buildSchema(`
+
+  type Medicine {
+    id: Int
+    name: String
+    description: String
+    productionDate: String
+    expiryDate: String
+    stored: String
+    lastUpdate: String
+    status: String
+  }
+
+  type Query {
+    medicines: [Medicine]
+    oneMedicine(id: Int): Medicine
+  }
+
+  type Mutation {
+    addMedicine(name: String, description: String, productionDate: String, expiryDate: String): Medicine
+    updateMedicine(id: Int, name: String, description: String, productionDate: String, expiryDate: String): Medicine
+    deleteMedicine(id: Int)
+  }
+`);
+
+const root = {
+  getMedicines: async () => {
+    const response = await fetch('https://backend-get-all-medicines-olz2xjbmza-uc.a.run.app/');
+    const data = await response.json();
+    return data;
+  },
+  getOneMedicine: ({id}) => {
+    return medicines.find(medicine => medicine.id === id);
+  },
+  addMedicine: ({name, description, productionDate, expiryDate}) => {
+    const id = medicines.length + 1;
+    const medicine = {id, name, description, productionDate, expiryDate, stored: 'yes', lastUpdate: new Date().toISOString(), status: 'active'};
+    medicines.push(medicine);
+    return medicine;
+  },
+  updateMedicine: ({id, name, description, productionDate, expiryDate}) => {
+    const medicine = medicines.find(medicine => medicine.id === id);
+    medicine.name = name;
+    medicine.description = description;
+    medicine.productionDate = productionDate;
+    medicine.expiryDate = expiryDate;
+    medicine.lastUpdate = new Date().toISOString();
+    return medicine;
+  },
+  deleteMedicine: ({id}) => {
+    medicines = medicines.filter(medicine => medicine.id !== id);
+  }
+}
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
